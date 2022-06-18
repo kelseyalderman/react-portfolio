0ec6ee82763afd0b1e45af727c3fc5d8be150f7c
Kelsey Alderman <kelseyalderman@gmail.com>
2022-06-18 15:18:00 -0700

    header and navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import About from "./components/About";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
 
 function App() {
+  const [pages] = useState([
+    { name: "about me" },
+    { name: "portfolio" },
+    { name: "contact" },
+    { name: "resume" },
+  ]);
+
+  const [currentPage, setCurrentPage] = useState(pages[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header>
+        <Nav
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        ></Nav>
+      </Header>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
