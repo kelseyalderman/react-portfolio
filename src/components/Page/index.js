2e2e55674d4d9f861ed79bf2585ce6503c934209
Kelsey Alderman <kelseyalderman@gmail.com>
2022-06-18 18:02:40 -0700

    style contact form

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import PageContent from "../PageContent";
 import About from "../About";
+import Contact from "../Contact";
 
 function Page({ currentPage }) {
   const renderPage = () => {
     switch (currentPage.name) {
       case "about me":
         return <About />;
+      case "contact":
+        return <Contact />;
       default:
         return <About />;
     }
